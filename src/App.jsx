7291cfde43adc9dd9c5ca11398a8ab76bc363f73
Patricio Ruiz <patoruiz998@gmail.com>
2022-04-21 13:16:08 -0300

    Responsive-desktop

--- src/App.jsx
+++ src/App.jsx
@@ -32,23 +32,26 @@ const All = ({ items, dispatch }) => {
     });
   };
   return (
-    <ul>
-      {
-        items.map(({ task, id, active, completed }, index) => (
-          <li key={index + 1}>
-            <label htmlFor={id} className={!active ? 'line-through font-bold' : undefined}>
-              <input type="checkbox" name={task} id={id} key={id} checked={!active} onChange={() => handleCheckChange(id)} /> {/* () => handleCheckChange(id) para pasarle un argumento en especifico*/}
-              {task}
-            </label>
-            {/* <input type="checkbox" name={task} id={id} key={id} checked={completed} onChange={() => handleCheckChangeCompleted(id)} /> */}
-            <br />
-            Activo: {active ? "Activo" : "No Activo"} <br />
-            Completado: {completed ? "Completado" : "No Completado"}<br />
-            <hr />
-          </li>
-        ))
-      }
-    </ul>
+    <div className='w-full mx-auto my-8'>
+      <ul>
+        {
+          items.map(({ task, id, active, completed }, index) => (
+            <li key={index + 1} className='flex flex-row mb-2 h-full w-full md:mb-3 lg:mb-5 xl:mb-7'>
+              <input type="checkbox" name={task} id={id} key={id} checked={!active} onChange={() => handleCheckChange(id)}
+                className='h-8 w-8 mx-5 lg:h-14 lg:w-14'
+              /> {/* () => handleCheckChange(id) para pasarle un argumento en especifico*/}
+              <label htmlFor={id} className={!active ? 'line-through font-bold m-auto w-full h-auto' : undefined}>
+                <span className='align-middle justify-center text-left md:text-lg lg:text-3xl'>
+                  {task}
+                </span>
+              </label>
+              {/* <input type="checkbox" name={task} id={id} key={id} checked={completed} onChange={() => handleCheckChangeCompleted(id)} /> */}
+              <hr />
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   );
 };
 
@@ -61,31 +64,34 @@ const Active = ({ items, dispatch }) => {
     });
   };
   return (
-    <ul>
-      {
-        items.map(({ id, task, active, completed }, index) => {
-          if (active) {
+    <div className='w-full mx-auto my-8'>
+      <ul>
+        {
+          items.map(({ id, task, active, completed }, index) => {
+            if (active) {
+              return (
+                <li key={index + 1} className='flex flex-row mb-2 h-full w-full md:mb-3 lg:mb-5 xl:mb-7'>
+                  <input type="checkbox" name={task} id={id} key={id} checked={!active} onChange={() => handleCheckChange(id)}
+                    className='h-8 w-8 mx-5 lg:h-14 lg:w-14'
+                  />
+                  <label htmlFor={id} className='w-full h-auto m-auto'>
+                    <span className='align-middle justify-center text-left md:text-lg lg:text-3xl'>
+                      {task}
+                    </span>
+                  </label>
+                  <hr />
+                </li>
+              )
+            }
             return (
-              <li key={index + 1}>
-                <label htmlFor={id}>
-                  <input type="checkbox" name={task} id={id} key={id} checked={!active} onChange={() => handleCheckChange(id)} />
-                  {task}
-                </label>
-                <br />
-                Activo: {active ? "Activo" : "No Activo"} <br />
-                Completado: {completed ? "Completado" : "No Completado"}<br />
-                <hr />
+              <li>
+
               </li>
             )
-          }
-          return (
-            <li>
-
-            </li>
-          )
-        })
-      }
-    </ul>
+          })
+        }
+      </ul>
+    </div>
   );
 };
 
@@ -111,23 +117,23 @@ const Completed = ({ items, dispatch }) => {
   };
   return (
     <div className='w-full mx-auto my-8'>
-      <ul className=''>
+      <ul>
         {
           items.map(({ id, task, active, completed }, index) => {
             if (completed) return (
-              <li key={index + 1} className='flex flex-row justify-between h-full w-full'>
+              <li key={index + 1} className='flex flex-row mb-2 justify-between h-full w-full md:mb-3 lg:mb-5 xl:mb-7'>
                 <input type="checkbox" name={task} id={id} key={id} checked disabled
-                  className='h-8 w-8 mx-5 disabled:opacity-40'
+                  className='h-8 w-8 mx-5 disabled:opacity-40 lg:h-14 lg:w-14'
                 />
                 <label className='w-full h-auto opacity-40 line-through font-bold m-auto' >
-                  <span className='align-middle justify-center text-left'>
+                  <span className='align-middle justify-center text-left md:text-lg lg:text-3xl'>
                     {task}
                   </span>
                 </label>
                 {/* <input type="checkbox" name={task} id={id} key={id} checked={completed} onChange={() => handleCheckChangeCompleted(id)} /> */}
                 <button onClick={() => deleteTask(id)} className='w-auto h-auto'>
                   <svg xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-auto stroke "
+                    className="h-5 w-auto stroke md:h-7 lg:h-14 "
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
@@ -148,13 +154,13 @@ const Completed = ({ items, dispatch }) => {
         <section className='flex justify-center content-center mt-3'>
           <button onClick={deleteAll}
             className='border border-red-600 bg-red-600 rounded-md 
-            w-auto p-2 m-2 text-center font-bold flex items-center '
+            w-auto p-2 m-2 text-center font-bold flex items-center md:p-3 xl:p-4'
           >
-            <span className='mx-3'>
+            <span className='mx-3 md:text-xl lg:text-3xl xl:text-5xl'>
               Clean
             </span>
             <svg xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-auto stroke "
+              className="h-5 w-auto stroke md:h-7 lg:h-14 xl:h-16 "
               viewBox="0 0 20 20"
               fill="currentColor"
             >
@@ -184,7 +190,7 @@ const App = () => {
     return (
       <NavLinkCustomize
         {...props}
-        className={({ isActive }) => isActive ? 'border-b-2 font-bold' : undefined}
+        className={({ isActive }) => isActive ? 'border-b-2 font-bold lg:border-b-4 xl:border-b-8' : undefined}
         to={to}
       >{children}
       </NavLinkCustomize>
@@ -213,26 +219,27 @@ const App = () => {
   return (
     <div
       className=' flex flex-col m-3 p-4 pb-1 h-fit content-center justify-center bg-[#1a1c22] rounded-lg shadow shadow-[#1a1c22]
-      sm:m-auto sm:p-6 sm:w-10/12'
+      sm:m-auto sm:p-6 sm:w-10/12
+      lg:p-4 lg:w-3/4'
     >
-      <nav className='my-6 mx-2 w-full border-b'>
-        <ul className='flex flex-row items-center justify-between'>
-          <li className='m-4 w-full text-center text-xl'>
+      <nav className='my-3 mx-2 w-full border-b lg:border-b-2 lg:min-w-fit lg:mb-5'>
+        <ul className='flex flex-row items-center justify-between lg:mb-5'>
+          <li className='m-4 w-full text-center text-xl md:text-3xl lg:text-4xl lg:pb-4 lg:m-0 xl:text-5xl'>
             <NavLink to="/">All</NavLink>
           </li>
-          <li className='m-4 w-full text-center text-xl'>
+          <li className='m-4 w-full text-center text-xl md:text-3xl lg:text-4xl lg:pb-4 lg:m-0 xl:text-5xl'>
             <NavLink to="/active">
               Active
             </NavLink>
           </li>
-          <li className='m-4 w-full text-center text-xl'>
+          <li className='m-4 w-full text-center text-xl md:text-3xl lg:text-4xl lg:pb-4 lg:m-0 xl:text-5xl'>
             <NavLink to="/completed">
               Completed
             </NavLink>
           </li>
         </ul>
       </nav>
-      <div className='flex w-full mx-auto my-3 justify-center md:max-h-28' >
+      <div className='flex w-full mx-auto my-3 justify-center md:max-h-28 lg:max-h-32' >
         <input
           type="text"
           className='
@@ -240,7 +247,7 @@ const App = () => {
         bg-[#1a1c22] border border-[#5fd9fb]
         shadow shadow-[#5fd9fb]
         focus:outline-none focus-within:border-2 p-2
-        md:h-11 md:w-full
+        md:h-11 md:w-full md:text-xl lg:h-16 lg:text-3xl xl:mx-5
         '
           value={inputValue}
           onChange={handleChange} />
@@ -248,7 +255,7 @@ const App = () => {
           className='
         transition-all ease-out duration-200
         w-1/3 mx-2 bg-[#5fd9fb] text-[#181a1b] font-bold rounded-md
-        active:bg-[#4db7d4] md:min-h-full md:w-1/4'
+        active:bg-[#4db7d4] md:min-h-full md:w-1/4 md:text-xl lg:text-3xl'
           onClick={handleClick} >
           Submit
         </button>
