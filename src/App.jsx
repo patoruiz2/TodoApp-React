45000c125e19ab152e809134213015ffc5b7ae1c
Patricio Ruiz <patoruiz998@gmail.com>
2022-04-20 23:30:45 -0300

    Responsive-640-768

--- src/App.jsx
+++ src/App.jsx
@@ -119,14 +119,25 @@ const Completed = ({ items, dispatch }) => {
                 <input type="checkbox" name={task} id={id} key={id} checked disabled
                   className='h-8 w-8 mx-5 disabled:opacity-40'
                 />
-                <label className='w-full h-auto opacity-40 line-through font-bold' >
+                <label className='w-full h-auto opacity-40 line-through font-bold m-auto' >
                   <span className='align-middle justify-center text-left'>
                     {task}
                   </span>
                 </label>
                 {/* <input type="checkbox" name={task} id={id} key={id} checked={completed} onChange={() => handleCheckChangeCompleted(id)} /> */}
                 <button onClick={() => deleteTask(id)} className='w-auto h-auto'>
-                  Delete
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-auto stroke "
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 
+              2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 
+              100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                      clipRule="evenodd" />
+                  </svg>
                 </button>
               </li>
             )
@@ -134,24 +145,30 @@ const Completed = ({ items, dispatch }) => {
         }
         <br />
         <hr />
-        <button onClick={deleteAll}
-          className='border border-red-600 bg-red-600 rounded-md 
-          w-1/6 p-2 m-2 text-center font-bold flex items-center justify-between'
-        >Clean
-          <svg xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-fit stroke "
-            viewBox="0 0 20 20"
-            fill="currentColor"
+        <section className='flex justify-center content-center mt-3'>
+          <button onClick={deleteAll}
+            className='border border-red-600 bg-red-600 rounded-md 
+            w-auto p-2 m-2 text-center font-bold flex items-center '
           >
-            <path
-              fillRule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 
-            2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 
-            100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clipRule="evenodd" />
-          </svg>
+            <span className='mx-3'>
+              Clean
+            </span>
+            <svg xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-auto stroke "
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 
+              2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 
+              100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clipRule="evenodd" />
+            </svg>
 
-        </button>
+          </button>
+
+        </section>
       </ul>
 
     </div>
@@ -195,46 +212,43 @@ const App = () => {
   // rounded-lg
   return (
     <div
-      className=' flex flex-col m-3 p-5 h-full content-center bg-[#1a1c22] rounded-lg shadow shadow-[#1a1c22]
-
-      
-    '
+      className=' flex flex-col m-3 p-4 pb-1 h-fit content-center justify-center bg-[#1a1c22] rounded-lg shadow shadow-[#1a1c22]
+      sm:m-auto sm:p-6 sm:w-10/12'
     >
       <nav className='my-6 mx-2 w-full border-b'>
         <ul className='flex flex-row items-center justify-between'>
-          <li className='m-4 w-full text-center'>
+          <li className='m-4 w-full text-center text-xl'>
             <NavLink to="/">All</NavLink>
           </li>
-          <li className='m-4 w-full text-center'>
+          <li className='m-4 w-full text-center text-xl'>
             <NavLink to="/active">
               Active
             </NavLink>
           </li>
-          <li className='m-4 w-full text-center'>
+          <li className='m-4 w-full text-center text-xl'>
             <NavLink to="/completed">
               Completed
             </NavLink>
           </li>
         </ul>
       </nav>
-      <div className='flex w-full mx-auto my-3 justify-center' >
+      <div className='flex w-full mx-auto my-3 justify-center md:max-h-28' >
         <input
           type="text"
           className='
-        w-1/2 mx-2 h-10 rounded-md 
+        w-1/2 mx-2 h-8 rounded-md 
         bg-[#1a1c22] border border-[#5fd9fb]
         shadow shadow-[#5fd9fb]
         focus:outline-none focus-within:border-2 p-2
+        md:h-11 md:w-full
         '
           value={inputValue}
           onChange={handleChange} />
         <button
           className='
-        transition-all ease-in-out duration-200
-        w-1/6 mx-2 bg-[#5fd9fb] text-[#181a1b] font-bold rounded-md
-        hover:bg-[#181a1b] hover:text-[#5fd9fb]
-        active:bg-[#2a2c2c] 
-        '
+        transition-all ease-out duration-200
+        w-1/3 mx-2 bg-[#5fd9fb] text-[#181a1b] font-bold rounded-md
+        active:bg-[#4db7d4] md:min-h-full md:w-1/4'
           onClick={handleClick} >
           Submit
         </button>
